af057b4f80a66671d903c77e4a3657856160bb11
Ryan Smick <ryan.smick@airbnb.com>
2022-09-21 09:59:30 -0700

    Remove filter lookup by name
    Use this to test out our filter types.

--- source/common/config/utility.h
+++ source/common/config/utility.h
@@ -311,18 +311,16 @@ public:
   template <class Factory, class ProtoMessage>
   static Factory* getAndCheckFactory(const ProtoMessage& message, bool is_optional) {
     Factory* factory = Utility::getFactoryByType<Factory>(message.typed_config());
-    if (Runtime::runtimeFeatureEnabled("envoy.reloadable_features.no_extension_lookup_by_name")) {
-      if (factory == nullptr && !is_optional) {
-        ExceptionUtil::throwEnvoyException(
-            fmt::format("Didn't find a registered implementation for '{}' with type URL: '{}'",
-                        message.name(), getFactoryType(message.typed_config())));
-      }
-      return factory;
-    } else if (factory != nullptr) {
-      return factory;
+    if (factory == nullptr && !is_optional) {
+      ExceptionUtil::throwEnvoyException(
+          fmt::format("Didn't find a registered implementation for '{}' with type URL: '{}'",
+                      message.name(), getFactoryType(message.typed_config())));
     }
-
-    return Utility::getAndCheckFactoryByName<Factory>(message.name(), is_optional);
+    else if (factory == nullptr) {
+      ENVOY_LOG_MISC(warn, fmt::format("WARNING: Didn't find a registered implementation for '{}' with type URL: '{}'",
+                                       message.name(), getFactoryType(message.typed_config())));
+    }
+    return factory;
   }
 
   /**
